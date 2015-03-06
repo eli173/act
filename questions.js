90c0295430060e7af89777075c5699dd9c0a6c35
eli173 <eli@eli173.com>
2015-03-06 16:44:14 -0600

    layout looks good now and basic functionality achieved

--- questions.js
+++ questions.js
@@ -12,17 +12,59 @@ var Question = function(question, options, answer, explanation=null)
 
 
 
-var sampleQ = Question("What's 3 + 4?",
-		       ['-1','12','7','34'],
-		       '7',
-		       "self-explanatory");
+Question.prototype.selected = function(choice)
+{
+    var choiceNum = choice.charCodeAt()-97;
+    if(this.options[choiceNum]==this.answer)
+    {
+	document.getElementById("results").hidden = false;
+	document.getElementById("results").innerHTML = "Good Job!";
+    }
+    else
+    {
+	document.getElementById("results").hidden = false;
+	document.getElementById("results").innerHTML =
+	    "Oops!</br>"+this.explanation;
+    }
+};
 
 
 
 Question.prototype.ask = function()
 {
+    var q = this;
+    console.log("bef")
+    console.log(this)
+    console.log("aft")
+    var ansPreHTML = '<div class="ansletter">'
+    var ansMidHTML = '</div> '
     document.getElementById("question").innerHTML = this.question;
-    document.getElementById("answers");
-    document.getElementById("a");
-    
-}
+    document.getElementById("answers").hidden = false;
+    document.getElementById("results").hidden = true;
+
+    document.getElementById("a").onclick = function(){q.selected("a");};
+    document.getElementById("b").onclick = function(){q.selected("b");};
+    document.getElementById("c").onclick = function(){q.selected("c");};
+    document.getElementById("d").onclick = function(){q.selected("d");};
+    document.getElementById("e").onclick = function(){q.selected("e");};
+
+    document.getElementById("a").innerHTML =
+	ansPreHTML+"A. "+ansMidHTML+this.options[0];
+    document.getElementById("b").innerHTML =
+	ansPreHTML+"B. "+ansMidHTML+this.options[1];
+    document.getElementById("c").innerHTML =
+	ansPreHTML+"C. "+ansMidHTML+this.options[2];
+    document.getElementById("d").innerHTML =
+	ansPreHTML+"D. "+ansMidHTML+this.options[3];
+    document.getElementById("e").innerHTML =
+	ansPreHTML+"E. "+ansMidHTML+this.options[4];
+};
+
+
+var sampleQ = new Question("What's 3 + 4?",
+			   ['-1','12','7','34','3/4'],
+		       '7',
+		       "self-explanatory");
+
+
+
