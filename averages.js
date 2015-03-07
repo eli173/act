58a31cefddf4f155e443a23353c03aa66952df81
eli173 <eli@eli173.com>
2015-03-07 02:33:55 -0600

    added last avgs prob type, maybe todo names and labels stuff, the math is good though I think

--- averages.js
+++ averages.js
@@ -8,8 +8,8 @@ var average = function(numls)
 
 var AveragesProb = function()
 {
-    return (Math.random()>0.5) ? AveragesType1() : AveragesType2();
-
+    return (Math.random()>1/3) ? AveragesType1() :
+	(Math.random()>0.5 ? AveragesType2() : AveragesType3());
 }
 
 
@@ -71,6 +71,51 @@ var AveragesType2 = function()
 	"on the last test?";
     // TODO
     var expl = "todo"
+    var q = new Question(qstring, opts, ans, expl);
+    return q;
+}
+
+
+var AveragesType3 = function()
+{
+    var qstring = "Your boss at the department store wants you to figure out the average price for the T-shirts in stock. You listed the prices of various shirts and the quantities in stock as follows:</br>"
+    var prices = [6.99];
+    for(var i =1;i<8;i++)
+    {
+	prices[i] = prices[i-1]+Math.round(Math.random()*4)+1;
+	prices[i] = Math.round(prices[i]*100)/100;
+    }
+    var qties = [];
+    for(var i=0;i<8;i++)
+    {
+	qties[i] = Math.round(Math.random()*6);
+    }
+    var i=0;
+    do
+    {
+	qstring +=String(prices[i])+": "+String(qties[i])+"</br>";
+	i++;
+    } while(i<8);
+    qstring += "Find the average price"
+    var tot = 0;
+    var num = 0;
+    for(var i=0;i<8;i++)
+    {
+	tot += prices[i]*qties[i];
+	num += qties[i];
+    }
+    var ans = Math.round(100*tot/num)/100;
+    var opts = [ans];
+    for(var i=0;i<4;i++)
+    {
+	opts.push(ans-5+ Math.round(Math.random()*100)/100);
+    }
+    for(var i=0;i<opts.length;i++)
+    {
+	opts[i] = Math.round(100*opts[i])/100;
+    }
+    var expl = "todo"
+    opts.sort(function(){return Math.random()>0.5;});
     var q = new Question(qstring, opts, ans, expl);
     return q;
 }
