4a6b04c3146a878dcf82d20705080a8b8da758b3
eli173 <eli@eli173.com>
2015-03-09 18:18:09 -0500

    fixed negative-answer issue everything works! (I think)

--- integer_algebra_prob.js
+++ integer_algebra_prob.js
@@ -15,6 +15,7 @@ var IntegerAlgebraProb = function()
 	    (coeffs[0]==0 && coeffs[1]==0) ||
 	    (coeffs[2]==0 && coeffs[3]==0) ||
 	    (coeffs[0]-coeffs[2]==0));
+    console.log(coeffs);
     var thevar = vars[Math.round(Math.random()*(vars.length-1))];
     var qstring = "Solve for <i>"+thevar+"</i>:</br>";
     var a = coeffs[0], b = coeffs[1], c = coeffs[2], d = coeffs[3];
@@ -34,12 +35,12 @@ var IntegerAlgebraProb = function()
     qstring += get_side_str(a,b,thevar) + 
 	" = " +
 	get_side_str(c,d,thevar);
-    var ans = new Rational(d-b,c-a);
+    var ans = new Rational(d-b,a-c);
     var opts = [];
     opts.push(ans);
     if(b-d!=0)
 	opts.push(new Rational(a-c,b-d));
-    opts.push(new Rational(b-d,c-a));
+    opts.push(new Rational(b-d,a-c));
     opts.push(a+b+c+d);
     while(opts.length<5)
     {
